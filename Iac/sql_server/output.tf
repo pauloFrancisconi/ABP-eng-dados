78dffc21907332bd2f5b68a23e2a29aff29cb273
pauloFrancisconi <pauloffrancisconi@gmail.com>
2025-06-15 15:38:04 -0300

    feat: estrutura modular com ADF, ADLS, Databricks e SQL Server

---
+++ Iac/sql_server/output.tf
@@ -0,0 +1,24 @@
+output "sqlserver_001" {
+  description = "Nome do SQL Server"
+  value       = azurerm_mssql_server.sql.name
+}
+
+output "database_001" {
+  description = "Nome do Banco de Dados"
+  value       = azurerm_mssql_database.sql.name
+}
+
+output "username" {
+  description = "Usuário SQL Server"
+  value       = var.usuario_admin
+}
+
+output "password" {
+  description = "Senha SQL Server"
+  value       = var.password
+}
+
+output "public_ip" {
+  description = "Meu IP Público"
+  value       = data.ipify_ip.public.ip_cidr
+}
---
+++ Iac/sql_server/variables.tf
@@ -0,0 +1,26 @@
+variable "resource_group_name" {
+  description = "Nome do Resource Group onde o SQL Server será criado"
+  type        = string
+}
+
+variable "location" {
+  description = "Região da Azure onde os recursos serão provisionados"
+  type        = string
+  default     = "East US"
+}
+
+variable "subscription_id" {
+  description = "ID da Subscription da Azure"
+  type        = string
+}
+
+variable "usuario_admin" {
+  description = "Login do administrador do SQL Server"
+  type        = string
+}
+
+variable "password" {
+  description = "Senha do administrador do SQL Server"
+  type        = string
+  sensitive   = true
+}
